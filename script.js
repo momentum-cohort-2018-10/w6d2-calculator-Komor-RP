bbb60b214f18d8d20b084e2e12b8394774d7a72e
Fan Huang <fan.huang.33@gmail.com>
2018-12-05 08:52:14 -0500

    calc working

--- script.js
+++ script.js
@@ -3,35 +3,67 @@ for (let button of buttons) {
     button.addEventListener("click", clickHandler);
 }
 
+let operation = "";
 let screenContents = "";
+var inputs = [];
 let screenDisplay = document.getElementById('screen');
 
-
-function updateScreen(term) {
-    screenContents = term;
-    screenDisplay.innerText = screenContents
-}
 
 function clickHandler(event) {
     let buttonContent = event.target.innerText;
     if (buttonContent.match(/\d/)) {
         screenContents += buttonContent;
         screenDisplay.innerText = screenContents;
-    }
-    if (buttonContent.match(/[x\/\+\-]/)) {
-        screenContents += buttonContent;
+    } else if ((buttonContent.match(/[X\/\+\-]/)) && operation){
+        enterPress(buttonContent);
+        operation = buttonContent;
+        inputs.push(screenContents);
+        screenContents = "";
+
+    } else if (buttonContent.match(/[X\/\+\-]/)){
+        inputs.push(screenContents);
+        operation = buttonContent;
+        screenContents = "";
         screenDisplay.innerText = screenContents;
 
-    }
-    if (buttonContent === "X") {
-        screenContents += '*';
-        screenDisplay.innerText = screenContents;
-    }
-    if (buttonContent === ".") {
+    } else if ((buttonContent === ".") && !(screenContents.includes("."))){
         screenContents += '.';
         screenDisplay.innerText = screenContents;
     }
-}   
+
+}
+
+function enterPress(input) {
+    inputs.push(screenContents);
+    inputs.forEach(input => Number(input));
+
+    switch(operation) {
+        case "+":
+            operation = "";
+            result = add(Number(inputs.pop()), Number(inputs.pop()));
+            break;
+        case "-":
+            operation = "";
+            let first = Number(inputs.pop());
+            let second = Number(inputs.pop());
+            result = subtract(second, first);
+            break;
+        case "/":
+            operation = "";
+            result = divide(Number(inputs.pop()), Number(inputs.pop()));
+            break;
+        case "X":
+            operation = "";
+            result = multiply(Number(inputs.pop()), Number(inputs.pop()));
+            break;
+        default:
+            result = Number(screenContents);
+            break;
+    }
+
+    screenContents = String(result);
+    screenDisplay.innerText = screenContents;
+}
 
 let clear = document.getElementById('clear');
 clear.addEventListener("click", function() {
@@ -39,10 +71,16 @@ clear.addEventListener("click", function() {
     screenDisplay.innerText = screenContents;
 });
 
-document.getElementById('equal').addEventListener("click", function(){
-    screenContents = eval(screenContents);
-    screenDisplay.innerText = screenContents;
+document.getElementById('equal').addEventListener("click", enterPress);
 
-})
+let add = (a,b) => a + b;
+let subtract = (a,b) => a - b;
+function divide(a, b) {
+    if (a == 0) {
+        return "ERROR";
+    }
+    return b / a;
+}
+let multiply = (a,b) => a * b;
 
-
+  
